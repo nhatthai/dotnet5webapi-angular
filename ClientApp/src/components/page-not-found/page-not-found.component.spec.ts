792698eadd835e0a5d5e65ee3d8d13ddca690c3b
nhatthai <nhat.thaiquang@asnet.com.vn>
2021-08-07 10:08:39 +0700

    Add page not found

--- ClientApp/src/components/page-not-found/page-not-found.component.spec.ts
+++ ClientApp/src/components/page-not-found/page-not-found.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthNavComponent } from './auth-nav.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
-describe('AuthNavComponent', () => {
-  let component: AuthNavComponent;
-  let fixture: ComponentFixture<AuthNavComponent>;
+describe('PageNotFoundComponent', () => {
+  let component: PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthNavComponent ]
+      declarations: [ PageNotFoundComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AuthNavComponent);
+    fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
